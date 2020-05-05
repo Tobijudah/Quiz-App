75a988ab69cc53ba61f552228dfe285935ce4d67
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-05-05 23:39:46 +0100

    minor fix

--- assets/js/end.js
+++ assets/js/end.js
@@ -5,17 +5,17 @@ let scoreMessage = document.getElementById('scoreMessage')
 scoreDisplay.textContent = score
 
 if (score == 50){
-   scoreMessage.textContent = 'Genius. You got all correct! Might as well start inventing time machines now \n :-)'
+   scoreMessage.innerHTML = 'Genius. You got all correct! Might as well start inventing time machines now <br> :-)'
 }
 
 if (score < 50 && score > 20){
-   scoreMessage.textContent = "Good job. You almost got it all. But you didn't. So i'm still smarter than you \n :-P"
+   scoreMessage.innerHTML = "Good job. You almost got it all. But you didn't. So i'm still smarter than you <br> :-P"
 }
 
 if (score < 30 && score > 0){
-   scoreMessage.textContent = "Nice.. but could be better. Actaully it's bad. Those were my simplest questions \n :-("
+   scoreMessage.innerHTML = "Nice.. but could be better. Actaully it's bad. Those were my simplest questions <br> :-("
 }
 
 if (score == 0){
-   scoreMessage.textContent = "Oops! You got none correct... try again? LOL. Please leave... and don't come back... ever \n :-!"
+   scoreMessage.innerHTML = "Oops! You got none correct... try again? LOL. Please leave... and don't come back... ever <br> :-!"
 }
